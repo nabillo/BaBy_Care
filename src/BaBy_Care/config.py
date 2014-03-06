b03e1802f9adaea790aad20855d14607e19b4655
nabillo <nabil.boukhari@gmail.com>
2014-03-06 20:25:33 +0000

    Change-Id: I307ecd64543cb84c0fb5fe214bf07c26c0b2686c

--- src/BaBy_Care/config.py
+++ src/BaBy_Care/config.py
@@ -6,6 +6,8 @@ Created on Feb 19, 2014
 
 SECRET_KEY= '?\xbf,\xb4\x8d\xa3"<\x9c\xb0@\x0f5\xab,w\xee\x8d$0\x13\x8b83'
 
+DEBUG = True
+
 CELERY_BROKER_URL= 'redis://localhost:6379',
 CELERY_RESULT_BACKEND= 'redis://localhost:6379'
 
@@ -17,3 +19,9 @@ AGI_NORMAL = 10
 REFRESH_RATE = 10
 
 ZODB_STORAGE = 'file://BaByCare.fs'
+
+UPLOAD_FOLDER = ''
+ALLOWED_EXTENSIONS = set(['mp3', 'mp4'])
+MAX_CONTENT_LENGTH = 16 * 1024 * 1024
+
+GPIO_CHANNEL = 23
