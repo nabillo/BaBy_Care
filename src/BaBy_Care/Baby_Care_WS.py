f1df247aab8398332a05e928c7928eb3179f125c
nabillo <nabil.boukhari@gmail.com>
2014-03-06 21:20:32 +0000

    Change-Id: Icb69de9e4dd1b4e70efbf94880824d39eaefd6ea

--- src/BaBy_Care/Baby_Care_WS.py
+++ src/BaBy_Care/Baby_Care_WS.py
@@ -9,10 +9,10 @@ from subprocess import Popen, PIPE
 from flaskext.zodb import ZODB
 
 import logging
-
+import Baby_Care_Stream
 import Baby_Care_Activity
 from Baby_Care_Activity import celery
-      
+
 # Initialize the Flask application
 app = Flask(__name__)
 app.config.from_object('config')
@@ -21,17 +21,17 @@ celery.conf.update(app.config)
 
 db = ZODB(app)
 
-with app.test_request_context():
-    if not db.has_key('lvl_normal'):
-      db['lvl_normal'] = app.config['LVL_NORMAL']
-    if not db.has_key('normal_interval'):
-      db['normal_interval'] = app.config['NORMAL_INTERVAL']
-    if not db.has_key('active_interval'):
-      db['active_interval'] = app.config['ACTIVE_INTERVAL']
-    if not db.has_key('agi_normal'):
-      db['agi_normal'] = app.config['AGI_NORMAL']
-  
-    
+with app.test_request_context() :
+	if not db.has_key('lvl_normal') :
+		db['lvl_normal'] = app.config['LVL_NORMAL']
+	if not db.has_key('normal_interval') :
+		db['normal_interval'] = app.config['NORMAL_INTERVAL']
+	if not db.has_key('active_interval') :
+		db['active_interval'] = app.config['ACTIVE_INTERVAL']
+	if not db.has_key('agi_normal') :
+		db['agi_normal'] = app.config['AGI_NORMAL']
+
+
 log = logging.getLogger(__name__)
 log.setLevel(logging.DEBUG)
 
@@ -47,56 +47,100 @@ log.addHandler(fh)
 from Baby_Care_Stream import steam_ctr_exe
 
 @app.route('/stream_ctr.json', methods=['POST'])
-def stream_ctr():
-  
-  log.debug('stream_ctr BEGIN')
-  
-  # Get the JSON data sent from the form
-  data = request.get_json(force=True)
-  
-  log.info(data['command'])
-  
-  try :
-    result = steam_ctr_exe(data['command'])
-  except Exception as e:
-    log.warning('steam_ctr_exe : %s', str(e))
-  
-  log.debug('stream_ctr END')
-  return jsonify(command=data['command'], result=result)
+def stream_ctr() :
+	"""Control streamer.
+	
+	@Imput    command : [Start,Stop,Restart].
+	@Return   result.
+	"""
+	
+	log.debug('stream_ctr BEGIN')
+	
+	# Get the JSON data sent from the form
+	data = request.get_json(force=True)
+	
+	log.info(data['command'])
+	if (command == 'Start') :
+		result = steam_ctr_start()
+	elif (command == 'Stop') :
+		result = steam_ctr_stop()
+	elif (command == 'Restart') :
+		result = steam_ctr_restart()
+	else :
+		log.info('invalid command')
+		result = 'None'
+	
+	log.debug('stream_ctr END')
+	return jsonify(result=result)
 
-# INPUTS  : command = Start/Stop/Calibrate
-#           agi_normal (decimal)
-# OUTPUTS : result
 @app.route('/activity_ctr.json', methods=['POST'])
-def activity_ctr():
-  log.debug('activity_ctr BEGIN')
-  
-  # Get the JSON data sent from the form
-  data = request.get_json(force=True)
-  
-  if (data['command'] == 'Start') :
-    # Start the agitation controller
-    agi_job = agitation_ctr_exe.delay()
-    result = agi_job.AsyncResult(agi_job.id).state
-    if (result == states.SUCCESS):
-      # Start the activity controller
-      act_job = activity_ctr_exe.delay()
-      result = act_job.AsyncResult(act_job.id).state
-  elif (data['command'] == 'Stop') :
-    # Stop the activity controller
-    revoke(act_job.id, terminate=True, signal='SIGTERM')
-  elif (data['command'] == 'Calibrate') :
-    # Calibrate the normale sound level
-    result = normal_levels(data['agi_normal'])
-    # TODO : adjust intervals
-    
-  
-  log.debug('activity_ctr END')
-  return jsonify(command=data['command'], result=result)
-  
-    
-if __name__ == '__main__':
-  app.run(
-      host="0.0.0.0",
-      port=int("12800")
-  )
+def activity_ctr() :
+	"""Control activity.
+	
+	@Imput    command : [Start,Stop,Calibrate].
+						agi_normal (decimal).
+	@Return   result.
+	"""
+	
+	log.debug('activity_ctr BEGIN')
+	
+	# Get the JSON data sent from the form
+	data = request.get_json(force=True)
+	
+	if (data['command'] == 'Start') :
+		# Start the agitation controller
+		agi_job = agitation_ctr_exe.delay()
+		result = agi_job.AsyncResult(agi_job.id).state
+		if (result == states.SUCCESS) :
+			# Start the activity controller
+			act_job = activity_ctr_exe.delay()
+			result = act_job.AsyncResult(act_job.id).state
+	elif (data['command'] == 'Stop') :
+		# Stop the activity controller
+		revoke(act_job.id, terminate=True, signal='SIGTERM')
+		# Stop the agitation controller
+		revoke(agi_job.id, terminate=True, signal='SIGTERM')
+	elif (data['command'] == 'Calibrate') :
+		# Calibrate the normale sound level
+		result = normal_levels(data['agi_normal'])
+		#TODO : adjust intervals
+	else :
+		log.info('invalid command')
+		result = 'None'
+	
+	log.debug('activity_ctr END')
+	return jsonify(result=result)
+	
+@app.route('/media_ctr.json', methods=['POST'])
+def media_ctr() :
+	"""Control media center.
+	
+	@Imput    command : [Upload,Delete,List,Play,Stop,VolUp,VolDown].
+						num (decimal).
+	@Return   result.
+	"""
+	
+	log.debug('media_ctr BEGIN')
+	
+	# Get the JSON data sent from the form
+	data = request.get_json(force=True)
+	
+	if (data['command'] == 'Upload') :
+		result = media_upload()
+	elif (data['command'] == 'Delete') :
+		result = media_del(data['num'])
+	elif (data['command'] == 'List') :
+		result = media_list()
+	else :
+		log.info('invalid command')
+		result = 'None'
+		
+	log.debug('media_ctr END')
+	return jsonify(result=result)
+	
+if __name__ == '__main__' :
+	app.run(
+			host="0.0.0.0",
+			port=int(app.config['PORT'])
+	)
+	
