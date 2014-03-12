096f08ccb860d8edb2f1ffe54c7e2088be084f95
nabillo <nabillo@users.noreply.github.com>
2014-03-12 11:22:29 +0000

    Create Streamer.sh

---
+++ src/Streamer.sh
@@ -0,0 +1,52 @@
+#! /bin/sh
+
+### BEGIN INIT INFO
+# Short-Description: Script to manage Streamer for BaBy Care
+# Description:       Script to manage Streamer for Baby Care by using the Gstreamer.
+### END INIT INFO
+
+gst_command = "gst-launch-1.0 v4l2src  ! \"video/x-raw,width=640,height=480,framerate=15/1\" ! \
+				omxh264enc target-bitrate=1000000 control-rate=variable ! \
+				video/x-h264,profile=high ! h264parse ! queue ! \
+				flvmux name=mux alsasrc device=hw:1 ! audioresample ! audio/x-raw,rate=48000 ! \
+				queue ! voaacenc bitrate=32000 ! queue ! mux. mux. ! \
+				rtmpsink location=\"rtmp://example.com/myapp/mystream live=1\"" 
+
+case "$1" in
+	start)
+		echo -n "Starting Streamer: gst-launch"
+		# Run Gstreamer
+		$gst_command
+		if [ $? -ne 0 ] then
+			echo -n "Error starting Streamer: gst-launch"
+			exit 1
+		fi
+		echo -n "."
+		;;
+	stop)
+		echo -n "Stoping Streamer: gst-launch"
+		# kill Gstreamer
+		killall gst-launch-1.0
+		if [ $? -ne 0 ] then
+			echo -n "Error stoping Streamer: gst-launch"
+			exit 2
+		fi
+		echo -n "."
+		;;
+	restart)
+		echo -n "Restarting Streamer: gst-launch"
+		killall -s SIGTERM gst-launch-1.0
+		if [ $? -ne 0 ] then
+			echo -n "Error stoping Streamer: gst-launch"
+			exit 2
+		fi
+		$gst_command
+		if [ $? -ne 0 ] then
+			echo -n "Error starting Streamer: gst-launch"
+			exit 1
+		fi
+		echo -n "."
+		;;
+esac
+
+exit 0
