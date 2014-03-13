946806b3d580596895cade79f8beb7d485a5bef4
nabillo <nabillo@users.noreply.github.com>
2014-03-13 17:05:05 +0000

    Update Baby_Care_Activity.py
    normalize logging

--- src/BaBy_Care/Baby_Care_Activity.py
+++ src/BaBy_Care/Baby_Care_Activity.py
@@ -15,6 +15,7 @@ import RPi.GPIO as GPIO
 
 # constants
 
+CARD        = 'sysdefault:CARD=Microphone'
 CHANNELS    = 1
 INFORMAT    = alsaaudio.PCM_FORMAT_FLOAT_LE
 RATE        = 44100
@@ -41,6 +42,7 @@ def sound_level() :
 	@Return   energy.
 	"""
 	
+	log.info('calculate sound level')
 	[length, data]=recorder.read()
 	# convert to an array of floats
 	floats = struct.unpack('f'*FRAMESIZE,data)
@@ -51,7 +53,7 @@ def sound_level() :
 	freq = aubio_pitchdetection(detect,buf)
 	# find energy of audio frame
 	energy = vec_local_energy(buf)
-	log.info("{:10.4f} {:10.4f}".format(freq,energy))
+	log.info("freq : {:10.4f} energy : {:10.4f}".format(freq,energy))
 	
 	return energy
 
@@ -62,55 +64,63 @@ def activity_check() :
 	@Return   .
 	"""
 	
+	log.info('Evaluate sound and agitation level')
 	energy = sound_level()
 	
 	if (energy < db['lvl_normal']) :
 		# Quiet state
-		log.info('Quiet state')
+		log.debug('Quiet state')
 		# We reset timer counter for agitation
 		refresh_count1 = 0
 		refresh_count2 = 0
 		
 	elif ((energy >= db['lvl_normal']) and (energy < db['lvl_normal'] + db['normal_interval'])) :
 		# Normal state
-		log.info('Normal state')
+		log.debug('Normal state')
 		refresh_count2 = 0
 		# If agitation stay higher than normal for a configurable periode 
 		if (db['mvt_count'] > db['agi_normal']) and (refresh_count1 > app.config['REFRESH_COUNT']) :
+			log.debug('if-refresh_count1 : %s',refresh_count1)
 			# We reduce the normal level
 			db['lvl_normal'] = db['lvl_normal'] * (app.config['REDUCTION_RATE']/100)
+			log.debug('new lvl_normal : %s',db['lvl_normal'])
 			refresh_count1 = 0
 			
 		# If agitation is higher we increment timer counter
 		elif (db['mvt_count'] > db['agi_normal']) :
 			refresh_count1 = refresh_count1 + 1
+			log.debug('elif-refresh_count1 : %s',refresh_count1)
 			
 		# If agitation return to normal we reset timer counter
 		else :
 			refresh_count1 = 0
+			log.debug('else-refresh_count1 : %s',refresh_count1)
 		
 	elif ((energy >= db['lvl_normal'] + db['normal_interval']) and (energy < db['lvl_normal'] + db['normal_interval'] + db['active_interval'])) :
 		# Active state
-		log.info('Active state')
+		log.debug('Active state')
 		# We reset timer counter for agitation
 		refresh_count1 = 0
 		# If active period is higher than a configurable periode 
 		if (refresh_count2 > app.config['REFRESH_COUNT']) :
-			# TODO : If agitation is higher we send silent notification
+			log.debug('if-refresh_count2 : %s',refresh_count2)
+			#TODO : If agitation is higher we send silent notification
 			# if (db['mvt_count'] > db['agi_normal']) :
 			# We increase the normal level
 			db['lvl_normal'] = db['lvl_normal'] * (1+(app.config['INCREASE_RATE']/100))
+			log.debug('new lvl_normal : %s',db['lvl_normal'])
 			refresh_count2 = 0
 		else :
 			refresh_count2 = refresh_count2 + 1
+			log.debug('else-refresh_count2 : %s',refresh_count2)
 		
 	elif (energy >= db['lvl_normal'] + db['normal_interval'] + db['active_interval']) :
 		# Criying state
-		log.info('Criying state')
+		log.debug('Criying state')
 		# We reset timer counter for agitation
 		refresh_count1 = 0
 		refresh_count2 = 0
-		# Tigger alarm
+		#TODO : Tigger alarm
 
 def agitation_count() :
 	"""Retreive number of mvt per minute and store it on db.
@@ -119,20 +129,23 @@ def agitation_count() :
 	@Return   .
 	"""
 	
+	log.info('Retreive number of mvt per minute')
 	global mvt_counter
-	log.info('agitation count : %d',mvt_counter)
+	log.debug('mvt_counter : %d',mvt_counter)
 	db['mvt_count'] = mvt_counter
 	mvt_counter = 0
 
 def mvt_counter(channel) :
-	"""incremente mvt conter on each call from GPIO triggering.
+	"""Incremente mvt conter on each call from GPIO triggering.
 	
 	@Imput    .
 	@Return   .
 	"""
 	
+	log.info('Incremente mvt conter')
 	global mvt_counter
 	mvt_counter = mvt_counter + 1
+	log.debug('mvt_counter : %d',mvt_counter)
 
 def agitation_detect() :
 	"""Detect agitation from GPIO and call mvt_counter.
@@ -141,6 +154,7 @@ def agitation_detect() :
 	@Return   .
 	"""
 	
+	log.info('Set GPIO')
 	GPIO.setmode(GPIO.BCM)
 	# GPIO 24 set up as an input, pulled down, connected to 3V3 on button press
 	GPIO.setup(app.config['GPIO_CHANNEL'], GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
@@ -153,6 +167,7 @@ def terminate() :
 	@Return   .
 	"""
 	
+	log.info('Free resources')
 	global recorder
 	global detect
 	global buf
@@ -165,7 +180,7 @@ def terminate() :
 	sys.exit(0)
 
 def handler(signum, frame) :
-	"""handle signals.
+	"""Signals handler.
 		SIGTERM to stop controller gracefully
 		SIGALRM handle activity controller alarm
 		SIGPROF handle agitation 
@@ -174,7 +189,8 @@ def handler(signum, frame) :
 	@Return   .
 	"""
 	
-	log.info('signal : %d',signum)
+	log.info('Signals handler')
+	log.debug('signal : %d',signum)
 	if (signum == signal.SIGTERM) :
 		terminate()
 	elif (signum == signal.SIGALRM) :
@@ -193,16 +209,17 @@ def activity_ctr_exe() :
 	global recorder
 	global detect
 	global buf
-
+	
+	log.info('Activity controller task')
 	refresh_count1 = 0
 	refresh_count2 = 0
 	
 	# Set the signal handler
 	signal.signal(signal.SIGTERM, handler)
 	signal.signal(signal.SIGALRM, handler)
-
+	
 	# set up audio input
-	card = 'sysdefault:CARD=Microphone'
+	card = CARD
 	recorder = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK, card)
 	recorder.setchannels(CHANNELS)
 	recorder.setrate(RATE)
@@ -224,6 +241,7 @@ def agitation_ctr_exe() :
 	@Return   .
 	"""
 	
+	log.info('Agitation controller task')
 	with app.app_context():
 		signal.setitimer(signal.ITIMER_PROF, 60, 60)
 		agitation_detect()
@@ -235,7 +253,9 @@ def normal_levels(agi_normal) :
 	@Return   .
 	"""
 	
+	log.info('Calibration')
 	db['agi_normal'] = agi_normal
 	db['lvl_normal'] = sound_level()
-	log.info("Normal level : {:10.4f}".format(db['lvl_normal']))
+	log.debug('agi_normal : %s',agi_normal)
+	log.debug("lvl_normal : {:10.4f}".format(db['lvl_normal']))
 
