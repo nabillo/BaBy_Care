76e3b077f564764a275d2a997befa1fd2f0da325
nabillo <nabillo@users.noreply.github.com>
2014-03-14 10:00:28 +0000

    Update __init__.py
    add rotating handler

--- src/BaBy_Care/__init__.py
+++ src/BaBy_Care/__init__.py
@@ -17,8 +17,8 @@ app.config.from_object('BaBy_Care.config')
 
 log = logging.getLogger(__name__)
 log.setLevel(app.config['LOG_LEVEL'])
-# add a file handler
-fh = logging.FileHandler(app.config['LOG_FILE'])
+# add a file rotation handler of 1Mb of size and 3 backups
+fh = logging.RotatingFileHandler(app.config['LOG_FILE'], maxBytes=1048576, backupCount=3)
 fh.setLevel(app.config['LOG_LEVEL'])
 # create a formatter and set the formatter for the handler.
 frmt = logging.Formatter('[%(asctime)s] - %(funcName)s - %(levelname)s - %(message)s')
