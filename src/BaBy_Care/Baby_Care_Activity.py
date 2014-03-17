9206d9522f6edfe665783b7b1d1b4896af89733e
nabillo <nabillo@users.noreply.github.com>
2014-03-17 17:17:46 +0000

    Update Baby_Care_Activity.py
    add exception to normal_levels

--- src/BaBy_Care/Baby_Care_Activity.py
+++ src/BaBy_Care/Baby_Care_Activity.py
@@ -230,8 +230,13 @@ def normal_levels(agi_normal) :
 	"""
 	
 	log.info('Calibration')
-	db['agi_normal'] = agi_normal
-	db['lvl_normal'] = sound_level()
-	log.debug('agi_normal : %s',agi_normal)
-	log.debug("lvl_normal : {:10.4f}".format(db['lvl_normal']))
-
+	try :
+		db['agi_normal'] = agi_normal
+		db['lvl_normal'] = sound_level()
+		log.debug('agi_normal : %s',agi_normal)
+		log.debug("lvl_normal : {:10.4f}".format(db['lvl_normal']))
+		result = 'Success'
+	except as e:
+		log.exception('Calibration error : %s',e.strerror)
+		result = 'Error'
+		
